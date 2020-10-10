a471f907569321e12220c50a9a745171d98c26b1
pippo98s <filippo5scarcella@gmail.com>
2020-10-10 19:11:30 +0200

    componenti collegati al router

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { TodoListComponent} from './todo-list/todo-list.component';
+import { AddTodoComponent} from './add-todo/add-todo.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {path: '' , redirectTo:'list' , pathMatch:'full'},
+  {path: 'list' , component: TodoListComponent},
+  {path: 'add'  , component: AddTodoComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TodoListComponent } from './todo-list/todo-list.component';
+import { AddTodoComponent } from './add-todo/add-todo.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TodoListComponent,
+    AddTodoComponent
   ],
   imports: [
     BrowserModule,
