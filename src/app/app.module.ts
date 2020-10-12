c3876a070e7b7ee4ba29c3431a6f9b4ebc0345a8
pippo98s <filippo5scarcella@gmail.com>
2020-10-12 15:26:03 +0200

    aggiunta la libreria  Material Angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule} from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TodoListComponent } from './todo-list/todo-list.component';
 import { AddTodoComponent } from './add-todo/add-todo.component';
 import { EditTodoComponent } from './edit-todo/edit-todo.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+const MaterialComponents = [
+  MatButtonModule,
+  MatSlideToggleModule
+]
 
 @NgModule({
   declarations: [
@@ -18,7 +26,12 @@ import { EditTodoComponent } from './edit-todo/edit-todo.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    MaterialComponents,
+    BrowserAnimationsModule
+  ],
+  exports: [
+    MaterialComponents
   ],
   providers: [],
   bootstrap: [AppComponent]
